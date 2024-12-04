a23d91525312dd677a3c4ad0aea4c3598702aca1
Hoang Trinh <hoanghz2004@gmail.com>
2024-12-04 08:24:05 +0700

    begin

--- lab03/OtherProjects/src/hust/soict/dsai/lab01/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-//eg2.2.1: HelloWorld.java
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hi\nguys!");
-        System.out.println("Hello \t World");
-    }   //end of method main
-}
